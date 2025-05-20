4e229c0544d09058f225f885deab0ae490950daa
Sergey Rimashevski <sergey.remash@gmail.com.com>
2025-05-20 17:34:03 +0300

    Extended Constants

--- core/src/main/java/io/github/asteroids_ta/constants/Constants.java
+++ core/src/main/java/io/github/asteroids_ta/constants/Constants.java
@@ -9,9 +9,16 @@ public class Constants {
     public static final float PLAYER_SPEED = 400f;
     public static final float PLAYER_DAMPING = 0.985f;
     public static final float PLAYER_THRUST = 30f;
+    public static final float PLAYER_SCALE = 1f;
+    public static final float PLAYER_ROTATION_OFFSET = 90f;
+    public static final float PLAYER_ROTATION_SPEED = 220f;
 
     public static final int INITIAL_LIVES = 3;
     public static final float ASTEROID_RADIUS_DIVISOR = 4f;
+    public static final float ASTEROID_SPAWN_INTERVAL = 2f;
+    public static final int MAX_ASTEROIDS = 10;
+    public static final float SCORE_INTERVAL = 1f;
+    public static final int SCORE_PER_INTERVAL = 10;
 
     public static final float FONT_SCALE = 2f;
 
@@ -24,4 +31,8 @@ public class Constants {
     public static final String GAME_OVER_TEXT = "Press SPACE to return to Menu or R to rerun";
     public static final float MENU_TITLE_OFFSET = 50f;
     public static final float MENU_TEXT_OFFSET = -50f;
+
+    public static final String ASSETS_PATH = "assets/PNG/";
+    public static final String PLAYER_SHIP_PATH = ASSETS_PATH + "playerShip1_blue.png";
+    public static final String METEORS_PATH = ASSETS_PATH + "Meteors/";
 }
