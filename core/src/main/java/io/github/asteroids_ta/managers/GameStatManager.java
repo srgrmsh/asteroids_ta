4fffbea6fd5008b096ce765e305a998f6da1a35b
Sergey Rimashevski <sergey.remash@gmail.com.com>
2025-05-20 17:08:36 +0300

    Added managers

--- core/src/main/java/io/github/asteroids_ta/managers/GameStatManager.java
+++ core/src/main/java/io/github/asteroids_ta/managers/GameStatManager.java
@@ -1,4 +1,30 @@
 package io.github.asteroids_ta.managers;
 
+import io.github.asteroids_ta.constants.Constants;
+
 public class GameStatManager {
+    private final ScoreManager scoreManager;
+    private final LifeManager lifeManager;
+
+    public GameStatManager(int startingLives) {
+        this.scoreManager = new ScoreManager();
+        this.lifeManager = new LifeManager(startingLives);
+    }
+
+    public ScoreManager getScoreManager() {
+        return scoreManager;
+    }
+
+    public LifeManager getLifeManager() {
+        return lifeManager;
+    }
+
+    public void reset() {
+        scoreManager.reset();
+        lifeManager.reset(Constants.INITIAL_LIVES);
+    }
+
+    public boolean isGameOver() {
+        return lifeManager.isDead();
+    }
 }
--- core/src/main/java/io/github/asteroids_ta/managers/LifeManager.java
+++ core/src/main/java/io/github/asteroids_ta/managers/LifeManager.java
@@ -1,4 +1,30 @@
 package io.github.asteroids_ta.managers;
 
 public class LifeManager {
+    private int lives;
+
+    public LifeManager(int initLives) {
+        this.lives = initLives;
+    }
+
+    public void reset(int initialLives) {
+        this.lives = initialLives;
+    }
+
+    public void loseLife(){
+        if (lives > 0){
+            lives--;
+        }
+    }
+
+    public int getLives() {
+        return lives;
+    }
+
+    public boolean isAlive(){
+        return lives > 0;
+    }
+    public boolean isDead(){
+        return lives <= 0;
+    }
 }
